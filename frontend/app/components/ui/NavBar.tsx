87fae5dc6417c34e843aaa0ecb8ee7ebeb3de366
mbyaf <rubberart7@users.noreply.github.com>
2025-09-14 15:53:32 -0400

    navbar issue fixed

--- frontend/app/components/ui/NavBar.tsx
+++ frontend/app/components/ui/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="flex flex-row justify-between items-center bg-white rounded-full p-4 w-full px-10 max-w-[1200px] mx-auto">
             <Link href="/">
                 <p className="text-2xl font-bold text-gradient">InternSight</p>
             </Link>
