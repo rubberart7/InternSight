c78eea6fa3f9aa264ca2d79aada9bab520ea84fc
mbyaf <rubberart7@users.noreply.github.com>
2025-12-25 17:51:52 -0500

    update packages

--- frontend/app/enter-resume/page.tsx
+++ frontend/app/enter-resume/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+"use client";
+
+import React, { use } from 'react';
 import Navbar from '../components/ui/NavBar';
 import ResumeAnalyzer from '../components/ui/ResumeAnalyzer';
+import { useAuth } from '../context/AuthContext';
+import LoadingSpinner from '../components/ui/LoadingSpinner';
 
 const page = () => {
+
+  const {loading} = useAuth();
+
+  if (loading) {
+		return (
+      <div className="flex flex-col items-center justify-center h-full min-h-screen p-8 bg-white text-gray-950">
+		<LoadingSpinner />
+		<p className="mt-4 text-lg">Loading...</p>
+		</div>
+    );
+	}
   
   return (
     <main className='bg-cover min-h-screen flex flex-col items-center'
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import LoginForm from '../components/auth/LoginForm'
 import Navbar from '../components/ui/NavBar'
